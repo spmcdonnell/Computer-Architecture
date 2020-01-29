2b3457ea830ccc05a7ad959ce42760e8ef8c493d
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2020-01-29 12:24:22 -0500

    Day 1 and 2 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,64 @@
 
 import sys
 
+
+# LOAD
+LDI = 0b10000010
+# Print
+PRN = 0b01000111
+# Multiply
+MUL = 0b10100010
+# Halt
+HTL = 0b00000001
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.pc = 0
+        self.reg = [0] * 8
 
     def load(self):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        if len(sys.argv) != 2:
+            print(f"Usage format: {sys.argv[0]} filename")
+            sys.exit(1)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        try:
+            with open(sys.argv[1]) as opened_file:
+                for line in opened_file:
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    num = line.split('#', 1)[0]
 
+                    self.ram[address] = int(num, 2)
+
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} was not found.")
+            sys.exit(2)
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+            return self.reg[reg_a]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +71,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +85,33 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        PRN = 0b01000111
+        LDI = 0b10000010
+        MUL = 0b10100010
+        HTL = 0b00000001
+
+        while True:
+
+            ir = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if ir == PRN:
+
+                print(self.reg[operand_a])
+                self.pc += 2
+
+            elif ir == LDI:
+
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            elif ir == MUL:
+
+                self.alu('MUL', operand_a, operand_b)
+                self.pc += 3
+
+            elif ir == HTL:
+
+                break
